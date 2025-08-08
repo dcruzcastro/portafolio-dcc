f9285beb8e3dc66fee719471a77c49ff6407d8f5
dcruzca <160550702+dcruzcastro@users.noreply.github.com>
2025-08-08 01:01:17 -0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const isProd = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
-  base: '/portafolio-dcc/',
-  plugins: [react()]
-});
+  plugins: [react()],
+  base: isProd ? '/portafolio-dcc/' : '/', 
+})
